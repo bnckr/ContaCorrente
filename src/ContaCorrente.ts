8c1a3a192f505ee82c13d51569d890e4a3d4913f
alexandro <alexandro.almeida@unosolucoes.com.br>
2022-08-22 15:41:46 -0300

    Ajustes por Alexandro

--- src/ContaCorrente.ts
+++ src/ContaCorrente.ts
@@ -1,28 +1,66 @@
-export class ContaCorrente {
-  agencia: number;
-  _saldo: number;
 
-  constructor(agencia: number, _saldo: number) {
+type Operacao = {
+  data: Date;
+  operacao: string,
+  valor: number;
+};
+
+export class ContaCorrente {
+  agencia: string;
+  _saldo: number;
+  historico: Operacao[];
+
+  constructor(agencia: string, _saldo: number) {
     this.agencia = agencia;
     this._saldo = _saldo;
+    this.historico = [];
   }
 
-  sacar(valor: number) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
+  sacar(valor: number, data: Date): number {
+    if (valor < 0) {
+      return 0.0;
     }
+    
+    if (this._saldo < valor) {
+      return 0.0;
+    }
+    
+    this._saldo -= valor;
+    this.historico.push(data);
+    return valor;
   }
 
-  depositar(valor: number) {
+  depositar(valor: number, data:Date): number {
     if (valor > 0) {
       this._saldo += valor;
+      this.historico.push(data);
       return valor;
     }
+
+    return 0.0;
   }
 
-  consultar(_saldo: number) {
+  consultar() {
     return this._saldo;
   }
+
+  exibirExtrato(): void {
+    this.historico.forEach((data)  => {
+      console.log(data)
+    });
+  }
 }
+
+
+const conta = new ContaCorrente("0001", 0.0);
+
+conta.depositar(100, new Date(2022, 1, 13));
+conta.depositar(100, new Date(2022, 1, 14));
+conta.sacar(50, new Date(2022, 1, 14));
+console.log(conta.consultar());
+conta.exibirExtrato();
+
+
+
+
 

f8497f4265de0673d5841d47614244b375d00025
Bianca Delbage <bnckr@outlook.com>
2022-08-23 10:30:32 -0300

    update

--- src/ContaCorrente.ts
+++ src/ContaCorrente.ts
@@ -1,8 +1,7 @@
-
 type Operacao = {
   data: Date;
-  operacao: string, //plus Permitir apenas operacoes válidas 
-  valor: number ;
+  operacao: "Depósito" | "Saque";
+  valor: number;
 };
 
 export class ContaCorrente {
@@ -16,59 +15,66 @@ export class ContaCorrente {
     this.historico = [];
   }
 
-  sacar(valor: number, data: Date): number {
+  sacar(valor: number): number {
     if (valor < 0) {
       return 0.0;
     }
-    
+
     if (this._saldo < valor) {
       return 0.0;
     }
-    
+
     this._saldo -= valor;
-    this.historico.push(data);
+    this.historico.push({ data: new Date(), operacao: "Saque", valor: valor });
     return valor;
   }
 
-  depositar(valor: number, data:Date): number {
-    if (valor > 0) {
-      this._saldo += valor;
-      this.historico.push(data);
-      return valor;
+  depositar(valor: number): number {
+    if (valor < 0) {
+      return 0.0;
     }
 
-    return 0.0;
+    this._saldo += valor;
+    this.historico.push({
+      data: new Date(),
+      operacao: "Depósito",
+      valor: valor,
+    });
+    return valor;
   }
 
   consultar() {
-    return this._saldo;
+    return this._saldo.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
   }
 
   //Exibir em formato de extrato
   exibirExtrato(): void {
-    this.historico.forEach((data)  => {
-      console.log(data)
+    this.historico.forEach((data) => {
+      console.log(data);
     });
   }
 
-    //Exibir em formato de extrato por periodo de tempo
-    exibirExtratoPorData(inicio: Date, fim: Date): void {
-      //Implementar
-      
-    }
-
+  //Exibir em formato de extrato por periodo de tempo
+  exibirExtratoPorData(inicio: Date, fim: Date): void {
+    //Implementar
+    const resultado = this.historico.filter(
+      (datas) => datas.data >= inicio && datas.data < fim
+    );
+    console.log(resultado);
+  }
 }
-
 
 const conta = new ContaCorrente("0001", 0.0);
 
-conta.depositar(100, new Date(2022, 1, 13));
-conta.depositar(100, new Date(2022, 1, 14));
-conta.sacar(50, new Date(2022, 1, 14));
+conta.depositar(100);
+conta.depositar(100);
+conta.sacar(50);
 console.log(conta.consultar());
 conta.exibirExtrato();
-
-
-
-
-
+conta.exibirExtratoPorData(
+  new Date("2022-08-22T00:00:00.000Z"),
+  new Date("2022-08-26T00:00:00.000Z")
+);
